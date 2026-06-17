21fe48da8fd2476d496ae144448e62bac1a32f15
Hafiz Waqasgit config --global user.email hafizwaqas,899@gmail.comgit config --global user.name Hafiz <hafizwaqas.899@gmail.com>
2026-06-18 03:40:04 +0500

    Fix CORS and Stripe config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_SECRET_KEY = "sk_test_xxxxxxxxxxxxxxxxx"
 STRIPE_PUBLIC_KEY = "pk_test_51SJWzDHFB4q3CuB6HVtp7VxMEiYRStDCndFXFisoN3kRPcKWdLtq6vzLDPbDbXG0GpTzr40sGjjM3tIYOstsTTys00TrIqhSqH"
 
 SECRET_KEY = 'django-insecure-tss-wsy&yyxy3y5imrpgc5$%hm(s5+g9-^fi8tonnno3*qeogw'
@@ -71,7 +71,7 @@ MIDDLEWARE = [
 # CORS
 # ========================
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
