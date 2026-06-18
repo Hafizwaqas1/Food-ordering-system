2642ae0a3aec38a075f4e35194067a08868b2a5f
Hafiz Waqasgit config --global user.email hafizwaqas,899@gmail.comgit config --global user.name Hafiz <hafizwaqas.899@gmail.com>
2026-06-19 04:08:43 +0500

    Fix media root in urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     # React frontend
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
