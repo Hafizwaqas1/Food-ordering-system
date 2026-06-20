e057413289b748c40d7a515e3c64f72b38ba11ab
Hafiz Waqasgit config --global user.email hafizwaqas,899@gmail.comgit config --global user.name Hafiz <hafizwaqas.899@gmail.com>
2026-06-20 09:53:25 +0500

    fix media in settings and imageUrl.js

--- frontend/src/utils/imageUrl.js
+++ frontend/src/utils/imageUrl.js
@@ -1,15 +1,3 @@
-// const BASE_URL = "https://hafiz899.pythonanywhere.com";
-
-// export const getImageUrl = (path) => {
-//   if (!path) return "/default.jpg";
-
-//   if (path.startsWith("http")) {
-//     return path;
-//   }
-
-//   return `${BASE_URL}${path}`;
-// };
-
 const BASE_URL = "https://hafiz899.pythonanywhere.com";
 
 export const getImageUrl = (path) => {
@@ -19,12 +7,5 @@ export const getImageUrl = (path) => {
     return path;
   }
 
-  // FIX DOUBLE /MEDIA/ ISSUE
-  let cleanPath = path;
-
-  if (cleanPath.startsWith("/media/media/")) {
-    cleanPath = cleanPath.replace("/media/media/", "/media/");
-  }
-
-  return `${BASE_URL}${cleanPath.startsWith("/") ? cleanPath : "/" + cleanPath}`;
+  return `${BASE_URL}${path}`;
 };
